65e005023242eabff012b2135be2c16a7ab3db7b
Jyoti Kataria <jyotikataria2008@gmail.com>
2021-06-28 11:46:53 +0530

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -4,7 +4,7 @@ Gene_Name	  Ex316-0HR-1	 Ex316-0HR-2	Ex316-12HR-1	Ex316-12HR-2	Ex316-6HR-1
 Lgals9	    0.502858	   0.473554	    0.84294	      0.852293	    2.45062
 Irx2	      2.28513	     3.06269	    2.86422	      3.39616	      0.960061
 
------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
 b=read.table("norm1.txt",sep="\t",header=T)
 
 row.names(b) <- b$Gene_Name
@@ -25,3 +25,4 @@ jpeg("Time0_vs_11day-tp_vs_others_Normalized_Counts.jpeg",width=3000,height=2000
 
 ggplot(data = b_reshape, aes(Genes, Expression)) + geom_boxplot() + geom_point() + scale_x_discrete(labels = function(x) str_wrap(x, width = 1)) + ggtitle("Time0 vs 11 day vs others") + ylab("log2(FPKM)") + xlab("Common genes") + theme(text = element_text(face="bold")) + theme(axis.text.x = element_text(angle = 90, hjust = 0.95, vjust = 0.2, size = 6)) + theme(plot.title = element_text(hjust = 0.5))
 dev.off()
+---------------------------------------------------------------------------------------------------------------------
