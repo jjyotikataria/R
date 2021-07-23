651f66cf574b79af6f051fa6785245510b03530a
Jyoti Kataria <jyotikataria2008@gmail.com>
2021-07-23 09:42:11 +0530

    Update regular_commands.R

--- regular_commands.R
+++ regular_commands.R
@@ -7,5 +7,14 @@ x <- matrix(rep(2:10), 3, 3)
 [2,]    3    6    9
 [3,]    4    7   10
 
+## Converting a sparse > dense > dataframe
+df <- as.data.frame(as.matrix(mat))
+
+
+## Dataframe > excel
+
+library(openxlsx)
+write.xlsx(df, "name.xlsx")
+
 
 
