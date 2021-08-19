1aad38bb06f0a28de64e6bce897c7d57cba2cf85
Jyoti Kataria <jyotikataria2008@gmail.com>
2021-08-19 07:10:49 +0530

    Update and rename boxplot.categorical.Rmd to boxplot.categorical.R

--- boxplot.categorical.R
+++ boxplot.categorical.R
@@ -22,6 +22,12 @@ data3$State.or.Province <- as.factor(data3$State.or.Province)
 boxplot(Revenue ~ State.or.Province, data = data3)
 
 
-```{r}
 ![](https://github.com/jjyotikataria/R/blob/main/figures/boxplot.png)
-```
+
+
+## Links to refer
+
+## https://r-coder.com/boxplot-r/
+
+
+## https://towardsdatascience.com/guide-to-dimensionality-reduction-in-single-cell-rna-seq-analysis-1d77284eed1c
