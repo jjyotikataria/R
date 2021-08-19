c5a154e0710c8a6a868f7d5d63b5510fed50cc22
Jyoti Kataria <jyotikataria2008@gmail.com>
2021-08-19 07:11:55 +0530

    Update boxplot.categorical.R

--- boxplot.categorical.R
+++ boxplot.categorical.R
@@ -1,10 +1,10 @@
 ## Example Data
-## City	            State or Province	 Revenue
+## City	            State or Province	Revenue
 ## Los Angeles	    CA	               27.38
 ## Los Angeles	    CA	               14.9
 ## Bremerton	    WA	               5.52
 ## Portland	    OR	               4.44
-## Beverly Hills   CA	               14
+## Beverly Hills    CA	               14
 
 ## Create side-by-side box plots for revenues broken down by state or province. Are these distributions essentially symmetric or skewed?
 
