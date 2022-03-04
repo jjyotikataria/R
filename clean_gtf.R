d0ec7d62772bd3d128c8caf894d12a4a7a9422e0
Jyoti Kataria <jyotikataria2008@gmail.com>
2022-03-04 10:18:25 +0530

    Update clean_gtf.R

--- clean_gtf.R
+++ clean_gtf.R
@@ -11,8 +11,8 @@
 
 library(dplyr)
 library(rtracklayer)
-gtf <- rtracklayer::import("file.gtf")
-final <- (as.data.frame(df@listData))
+gtf <- rtracklayer::import("GRCh38ERCC.ensembl91.gtf")
+final <- as.data.frame(gtf@elementMetadata@listData)
 final2 <- select(final,c("source","type","gene_id","gene_name","gene_biotype","transcript_id","transcript_id","transcript_name","transcript_biotype"))
 
 # Adding chromosome info in a separate column
@@ -26,4 +26,13 @@ final <- cbind(start_end,final2)
 
 # Arranging using dplyr select
 final <- select(final,c("chr","start","end","source","feature","gene_id","gene_name","gene_biotype","transcript_id","transcript_id","transcript_name","transcript_biotype","width"))
-write.csv(final,"cleaned_gtf.csv", row.names=FALSE)
+#write.csv(final,"cleaned_gtf.csv", row.names=FALSE)
+
+# Selecting only genes from the gtf 
+only_gene <- final[final$feature=="gene",]
+
+# Selecting pc genes from the gene list
+only_pc_genes <- only_gene[only_gene$gene_biotype=="protein_coding",]
+write.csv(final,"only_pc_genes_gtf.csv", row.names=FALSE)
+
+
