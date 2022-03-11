25b4cc1f67fff63053651b175d6063e3ba38d361
Jyoti Kataria <jyotikataria2008@gmail.com>
2022-03-11 11:48:06 +0530

    Update regular_commands.R

--- regular_commands.R
+++ regular_commands.R
@@ -17,6 +17,9 @@ mod <- melted %>%
 + theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=1)) 
 
 
+# Make the ggtitle centred
++ theme(plot.title = element_text(hjust=0.5))
+
 x <- matrix(rep(2:10), 3, 3)
 > x
      [,1] [,2] [,3]
