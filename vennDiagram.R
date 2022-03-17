84e37339d44f53716fe74316a5ed463ac9e985b7
Jyoti Kataria <jyotikataria2008@gmail.com>
2022-03-17 10:07:59 +0530

    Update vennDiagram.R

--- vennDiagram.R
+++ vennDiagram.R
@@ -46,46 +46,49 @@ voom.norm.data <- voom.norm$E
 data <- as.data.frame(voom.norm.data)
 data<-tibble::rownames_to_column(data,"gene_id")
 
-venn_diagram <- function(data,ref,x1,y1){
-  
-  q1<-quantile(data[,ref],0.25)
-  q2<-quantile(data[,ref],0.50)
-  q3<-quantile(data[,ref],0.75)
 
-  ref_q1<-filter(data,data[,ref]<=q1)$gene_id
-  ref_q2<-filter(data,data[,ref]>q1,data[,ref]<=q2)$gene_id
-  ref_q3<-filter(data,data[,ref]>q2,data[,ref]<=q3)$gene_id
-  ref_q4<-filter(data,data[,ref]>q3,data[,ref]<=max(data[,ref]))$gene_id
+## Creating the Venn Diagram function
+
+venn_diagram <- function(data,ref,x1){
   
-  x1_q1<-filter(data,data[,x1]<=q1)$gene_id
-  x1_q2<-filter(data,data[,x1]>q1,data[,x1]<=q2)$gene_id
-  x1_q3<-filter(data,data[,x1]>q2,data[,x1]<=q3)$gene_id
-  x1_q4<-filter(data,data[,x1]>q3,data[,x1]<=max(data[,x1]))$gene_id
+  q1_ref<-quantile(data[,ref],0.25)
+  q2_ref<-quantile(data[,ref],0.50)
+  q3_ref<-quantile(data[,ref],0.75)
+
+  q1_x1<-quantile(data[,x1],0.25)
+  q2_x1<-quantile(data[,x1],0.50)
+  q3_x1<-quantile(data[,x1],0.75)
   
-  y1_q1<-filter(data,data[,y1]<=q1)$gene_id
-  y1_q2<-filter(data,data[,y1]>q1,data[,y1]<=q2)$gene_id
-  y1_q3<-filter(data,data[,y1]>q2,data[,y1]<=q3)$gene_id
-  y1_q4<-filter(data,data[,y1]>q3,data[,y1]<=max(data[,y1]))$gene_id
+  ref_q1_genes <-filter(data,data[,ref]<=q1_ref)$gene_id
+  ref_q2_genes <-filter(data,data[,ref]>q1_ref,data[,ref]<=q2_ref)$gene_id
+  ref_q3_genes <-filter(data,data[,ref]>q2_ref,data[,ref]<=q3_ref)$gene_id
+  ref_q4_genes <-filter(data,data[,ref]>q3_ref,data[,ref]<=max(data[,ref]))$gene_id
   
-  list1 <- list(ref_q1,x1_q1,y1_q1)
-  list2 <- list(ref_q2,x1_q2,y1_q2)
-  list3 <- list(ref_q3,x1_q3,y1_q3)
-  list4 <- list(ref_q4,x1_q4,y1_q4)
+  x1_q1_genes <-filter(data,data[,x1]<=q1_x1)$gene_id
+  x1_q2_genes <-filter(data,data[,x1]>q1_x1,data[,x1]<=q2_x1)$gene_id
+  x1_q3_genes <-filter(data,data[,x1]>q2_x1,data[,x1]<=q3_x1)$gene_id
+  x1_q4_genes <-filter(data,data[,x1]>q3_x1,data[,x1]<=max(data[,x1]))$gene_id
+
   
-  a1<-venn.diagram(list1, col=c("red", 'green', 'blue'), cex=0.8,alpha=0.50,filename = NULL,
-                   category.names=c("FFPE5","FFPE2","FFPE1"),
+  list1 <- list(ref_q1_genes,x1_q1_genes)
+  list2 <- list(ref_q2_genes,x1_q2_genes)
+  list3 <- list(ref_q3_genes,x1_q3_genes)
+  list4 <- list(ref_q4_genes,x1_q4_genes)
+  
+  a1<-venn.diagram(list1, col=c("red", 'green'), cex=0.8,alpha=0.50,filename = NULL,
+                   category.names=c("FFPE5","FFPE2"),
                    print.mode=c("raw","percent"), main="Q1", cat.fontface = "bold",cat.cex=0.7)
   
-  b1<-venn.diagram(list2, col=c("red", 'green', 'blue'), cex=0.8,alpha=0.50,filename = NULL,
-                   category.names=c("FFPE5","FFPE2","FFPE1"),
+  b1<-venn.diagram(list2, col=c("red", 'green'), cex=0.8,alpha=0.50,filename = NULL,
+                   category.names=c("FFPE5","FFPE2"),
                    print.mode=c("raw","percent"), main="Q2", cat.fontface = "bold",cat.cex=0.7)
     
-  c1<-venn.diagram(list3, col=c("red", 'green', 'blue'), cex=0.8,alpha=0.50,filename = NULL,
-                   category.names=c("FFPE5","FFPE2","FFPE1"),
+  c1<-venn.diagram(list3, col=c("red", 'green'), cex=0.8,alpha=0.50,filename = NULL,
+                   category.names=c("FFPE5","FFPE2"),
                    print.mode=c("raw","percent"), main="Q3", cat.fontface = "bold",cat.cex=0.7)
   
-  d1<-venn.diagram(list4, col=c("red", 'green', 'blue'), cex=0.8,alpha=0.50,filename = NULL,
-                   category.names=c("FFPE5","FFPE2","FFPE1"),
+  d1<-venn.diagram(list4, col=c("red", 'green'), cex=0.8,alpha=0.50,filename = NULL,
+                   category.names=c("FFPE5","FFPE2"),
                    print.mode=c("raw","percent"), main="Q4", cat.fontface = "bold", cat.cex=0.7)
   
   pushViewport(plotViewport(layout=grid.layout(2, 2)))
@@ -102,5 +105,7 @@ venn_diagram <- function(data,ref,x1,y1){
   grid.draw(d1)
   
 }
+
+## Calling the function
 
 venn_diagram(data,"EA5045830","EA5045791","EA5045806")
